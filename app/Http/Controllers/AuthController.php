66ee0092e9b9644dea2124181cd47ee41ea39ab9
Juankno <juanes0202@gmail.com>
2025-04-08 14:57:16 -0500

    feat: Cargar el rol del usuario autenticado al obtener información del usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,6 +62,6 @@ class AuthController extends Controller
      */
     public function user(Request $request)
     {
-        return new UserResource($request->user());
+        return new UserResource($request->user()->load('role'));
     }
 }
